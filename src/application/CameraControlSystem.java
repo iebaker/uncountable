35075bfbace280e54c1064507d4adf34d1e7ec57
Izaak Baker <ibaker@palantir.com>
2015-08-14 16:37:46 -0700

    Switched to goal velocity based camera motion. A little jerky because of delay for onKeyRepeat

--- src/application/CameraControlSystem.java
+++ src/application/CameraControlSystem.java
@@ -8,13 +8,25 @@ import static org.lwjgl.glfw.GLFW.GLFW_KEY_W;
 import joml.Vector2f;
 import joml.Vector3f;
 import rendering.Camera;
+import rendering.Points;
 
 public class CameraControlSystem extends GameSystem {
 
-    private float m_movementSpeedFactor = 0.1f;
+    private Vector3f m_goalVelocity = new Vector3f(Points.ORIGIN_3D);
+    private Vector3f m_realVelocity = new Vector3f(Points.ORIGIN_3D);
+    private float m_accelerationFactor = 0.1f;
+    private float m_speed = 5.0f;
 
     public CameraControlSystem() {
         super();
+    }
+
+    @Override
+    public void tick(float seconds) {
+        Camera camera = Uncountable.game.getWorld().getCamera();
+        m_realVelocity.add(new Vector3f(m_goalVelocity).sub(m_realVelocity).mul(m_accelerationFactor));
+        camera.translate(new Vector3f(m_realVelocity).mul(seconds));
+        m_goalVelocity = Points.ORIGIN_3D;
     }
 
     @Override
@@ -28,25 +40,25 @@ public class CameraControlSystem extends GameSystem {
         float xDiff = (delta.x / width) * camera.get(Camera.FOV);
         float yDiff = (delta.y / height) * camera.get(Camera.FOV) / camera.get(Camera.ASPECT_RATIO);
 
-        camera.sub(Camera.YAW, xDiff);
-        camera.add(Camera.PITCH, yDiff);
+        camera.add(Camera.YAW, xDiff);
+        camera.sub(Camera.PITCH, yDiff);
     }
 
     @Override
-    public void onKeyHeld(int key) {
+    public void onKeyRepeat(int key) {
         Camera camera = Uncountable.game.getWorld().getCamera();
         switch(key) {
         case GLFW_KEY_W:
-            camera.translate(new Vector3f(camera.getHeading()).mul(m_movementSpeedFactor));
+            m_goalVelocity = new Vector3f(camera.getHeading()).mul(m_speed);
             break;
         case GLFW_KEY_A:
-            camera.translate(new Vector3f(camera.getRight()).mul(m_movementSpeedFactor));
+            m_goalVelocity = new Vector3f(camera.getRight()).mul(m_speed);
             break;
         case GLFW_KEY_S:
-            camera.translate(new Vector3f(camera.getHeading()).mul(-m_movementSpeedFactor));
+            m_goalVelocity = new Vector3f(camera.getHeading()).mul(-m_speed);
             break;
         case GLFW_KEY_D:
-            camera.translate(new Vector3f(camera.getRight()).mul(-m_movementSpeedFactor));
+            m_goalVelocity = new Vector3f(camera.getRight()).mul(-m_speed);
             break;
         }
     }
