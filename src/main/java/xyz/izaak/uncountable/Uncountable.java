f0e95cf62cd1b58777a4e2f0b7f1bacddf9af087
Izaak Baker <ibaker@palantir.com>
2016-08-17 14:21:13 -0700

    Moved existing src to src-legacy, created stub game to import radon Game object to prove concept

---
+++ src/main/java/xyz/izaak/uncountable/Uncountable.java
@@ -0,0 +1,10 @@
+package xyz.izaak.uncountable;
+
+import xyz.izaak.radon.core.Game;
+
+public class Uncountable {
+    public static void main(String... args) {
+        Game uncountable = new Game("Uncountable");
+        uncountable.run();
+    }
+}
